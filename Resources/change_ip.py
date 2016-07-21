fc6a394ff9fb67bd8a6f6cea0b5a90e4c2f96841
Yahya <yahyathai@gmail.com>
2016-07-21 11:37:40 -0400

    Updated some strings

--- Resources/change_ip.py
+++ Resources/change_ip.py
@@ -1,11 +1,11 @@
 os.system('cls')
-print("Please type in your new IP address.\n")
+print("Please type in your new Wii U IP address. E.G [192.168.1.101] or [10.0.0.3]\n")
 fresh_ip = raw_input(">> ")
 save = open("./Resources/save_ip.txt", "w")
 save.write(fresh_ip)
 save.close()
 ip = fresh_ip
 print("Your Wii U IP has been updated!")
-time.sleep(2)
+time.sleep(1.5)
 
 execfile("./Resources/menu.py")
